a372c998d7ca2e89c55ebdaa7d9d178f01a87cd1
celiawong0119 <68861940+celiawong0119@users.noreply.github.com>
2026-02-27 00:35:05 -0500

    refactor: add variant  to the Stars background

--- src/components/Visual/Stars/Stars.tsx
+++ src/components/Visual/Stars/Stars.tsx
@@ -2,14 +2,52 @@ import { FC, memo, useRef } from "react";
 import { motion } from "framer-motion";
 import styles from "./Stars.module.scss";
 
-const Stars: FC = () => {
-  // two slow layers for parallax drift
+type StarsVariant = "gold" | "white";
+interface StarsProps {
+  variant?: StarsVariant;
+}
+const Stars: FC<StarsProps> = ({ variant = "gold" }) => {
   return (
     <>
-      {/* far layer */}
-      <ParallaxStarLayer speed={90} shiftPct={-25} density={120} opacity={0.18} />
-      {/* near layer */}
-      <ParallaxStarLayer speed={60} shiftPct={-40} density={80} opacity={0.28} />
+      {variant === "gold" ? (
+        <>
+          {/* far layer */}
+          <ParallaxStarLayer
+            speed={90}
+            shiftPct={-25}
+            density={120}
+            opacity={0.18}
+            variant={variant}
+          />
+          {/* near layer */}
+          <ParallaxStarLayer
+            speed={60}
+            shiftPct={-40}
+            density={80}
+            opacity={0.28}
+            variant={variant}
+          />
+        </>
+      ) : (
+        <>
+          {/* far layer */}
+          <ParallaxStarLayer
+            speed={130}
+            shiftPct={-25}
+            density={420}
+            opacity={0.22}
+            variant={variant}
+          />
+          {/* near layer */}
+          <ParallaxStarLayer
+            speed={100}
+            shiftPct={-40}
+            density={160}
+            opacity={0.7}
+            variant={variant}
+          />
+        </>
+      )}
     </>
   );
 };
@@ -21,11 +59,13 @@ function ParallaxStarLayer({
   shiftPct,
   density,
   opacity,
+  variant,
 }: {
   speed: number; // seconds per loop
   shiftPct: number; // how far to drift (negative = left)
   density: number;
   opacity: number;
+  variant: StarsVariant;
 }) {
   return (
     <div className={styles.starsLayer}>
@@ -34,7 +74,7 @@ function ParallaxStarLayer({
         transition={{ duration: speed, repeat: Infinity, ease: "easeInOut" }}
         style={{ width: "140%", height: "100%" }}
       >
-        <StarField density={density} color={"#FFB93A"} opacity={opacity} />
+        <StarField density={density} opacity={opacity} variant={variant} />
       </motion.div>
     </div>
   );
@@ -42,22 +82,31 @@ function ParallaxStarLayer({
 
 const StarField = memo(function StarField({
   density,
-  color,
   opacity = 0.25,
+  variant = "gold",
 }: {
   density: number;
-  color: string;
   opacity?: number;
+  variant?: StarsVariant;
 }) {
   // generate ONCE, keep forever
   const dotsRef = useRef(
-    Array.from({ length: density }, () => ({
-      x: Math.random() * 120, // extend beyond right edge for drift room
-      y: Math.random() * 100,
-      r: Math.random() * 0.5 + 0.15, // [0.15, 0.65)
-      tw: Math.random() * 0.8 + 0.6, // twinkle duration 0.6s to 1.4s
-      dly: Math.random() * 2, // // 0 to 2s delay
-    })),
+    Array.from({ length: density }, () => {
+      const isGold = variant === "gold";
+
+      return {
+        x: Math.random() * 120, // extend beyond right edge for drift room
+        y: Math.random() * 100,
+        r: isGold
+          ? Math.random() * 0.5 + 0.15 // [0.15, 0.65)
+          : Math.random() * 0.1 + 0.08,
+        tw: isGold
+          ? Math.random() * 0.8 + 0.6 // twinkle duration 0.6s to 1.4s
+          : Math.random() * 2.6 + 2.2,
+        dly: Math.random() * 2, // // 0 to 2s delay}
+        fill: isGold ? "#FFB93A" : "rgba(255,255,255,0.95)",
+      };
+    }),
   );
   const dots = dotsRef.current;
 
@@ -73,7 +122,7 @@ const StarField = memo(function StarField({
           cx={d.x}
           cy={d.y}
           r={d.r}
-          fill={color}
+          fill={d.fill}
           initial={{ opacity: 0.15 }}
           animate={{ opacity: [0.15, opacity, 0.15] }}
           transition={{ duration: d.tw, repeat: Infinity, ease: "easeInOut", delay: d.dly }}
